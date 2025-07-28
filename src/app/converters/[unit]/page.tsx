1e1a565afcc13446b1f3883744070f501a3f21a9
converthaven12 <mtxbot882@gmail.com>
2025-07-27 22:53:28 -0600

    Update page.tsx

--- src/app/converters/[unit]/page.tsx
+++ src/app/converters/[unit]/page.tsx
@@ -1,17 +1,14 @@
-import React from "react";
-import KgToGram from "./WeightAndMass/KgToGram/page";
-import ConverterHome from "@/app/utils/components/ConverterHome/ConverterHome";
-// 3 levels up: [unit] → converters → app, then into helper
-import { conversionFactors } from "../../../helper/conversionFactors";
+// File: src/app/converters/[unit]/page.tsx
 
-const Page: React.FC = () => {
-  // Example: you can now use conversionFactors here if needed
+import React from "react";
+import ConverterHome from "@/app/utils/components/ConverterHome/ConverterHome";
+
+const UnitPage: React.FC = () => {
   return (
     <div>
       <ConverterHome />
-      {/* If you need direct conversions, use conversionFactors */}
     </div>
   );
 };
 
-export default Page;
+export default UnitPage;
