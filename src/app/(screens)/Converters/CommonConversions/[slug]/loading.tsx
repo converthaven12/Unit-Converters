9138194a6a5868497205ba1795876e0a936d9d21
converthaven12 <mtxbot882@gmail.com>
2025-12-22 18:07:27 +0500

    Add loading component for converter screen

---
+++ src/app/(screens)/Converters/CommonConversions/[slug]/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div style={{ padding: 16 }}>
+      <div style={{ height: 140, borderRadius: 10, background: "#f2f2f2" }} />
+      <p>Loading converter...</p>
+    </div>
+  );
+}
