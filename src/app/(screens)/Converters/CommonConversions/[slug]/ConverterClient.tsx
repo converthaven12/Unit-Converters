13c6537eedd260bdfc642a87d84b09e68562c791
converthaven12 <mtxbot882@gmail.com>
2025-12-22 21:56:42 +0500

    Refactor ConversionData type to Conversion with required fields

--- src/app/(screens)/Converters/CommonConversions/[slug]/ConverterClient.tsx
+++ src/app/(screens)/Converters/CommonConversions/[slug]/ConverterClient.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-type ConversionData = {
-  title?: string;
-  description?: string;
-  from?: string;
-  to?: string;
+type Conversion = {
+  title: string;
+  description: string;
+  from: string;
+  to: string;
 };
 
-export default function ConverterClient({ data }: { data: any }) {
-return (
+export default function ConverterClient({ data }: { data: Conversion }) {
+  return (
     <div style={{ marginTop: 20 }}>
       <p>
-        From <b>{data?.from}</b> to <b>{data?.to}</b>
+        From <b>{data.from}</b> to <b>{data.to}</b>
       </p>
     </div>
   );
