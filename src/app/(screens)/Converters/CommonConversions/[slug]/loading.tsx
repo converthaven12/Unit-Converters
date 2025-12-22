d2731c152dac44394896ada887cd1b6af937c2c3
converthaven12 <mtxbot882@gmail.com>
2025-12-22 23:01:16 +0500

    Delete src/app/(screens)/Converters/CommonConversions/[slug] directory

--- src/app/(screens)/Converters/CommonConversions/[slug]/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-  return (
-    <div style={{ padding: 16 }}>
-      <div style={{ height: 140, borderRadius: 10, background: "#f2f2f2" }} />
-      <p>Loading converter...</p>
-    </div>
-  );
-}
