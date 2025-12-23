ac3a7d5bc2629174dcf293f2583f01469c61f0f1
converthaven12 <mtxbot882@gmail.com>
2025-12-23 10:44:00 +0500

    Enhance ConverterClient with description field
    Added a description field to the ConversionData type and updated the component to display it.

--- src/app/(screens)/Converters/CommonConversions/[slug]/ConverterClient.tsx
+++ src/app/(screens)/Converters/CommonConversions/[slug]/ConverterClient.tsx
@@ -2,21 +2,20 @@
 
 type ConversionData = {
   title: string;
+  description: string;
   from: string;
   to: string;
 };
 
-export default function ConverterClient({
-  data,
-}: {
-  data: ConversionData;
-}) {
+export default function ConverterClient({ data }: { data: ConversionData }) {
   return (
-    <div style={{ padding: 20 }}>
-      <h1>{data.title}</h1>
-      <p>
+    <div style={{ padding: 16 }}>
+      <h1 style={{ fontSize: 24, fontWeight: 700 }}>{data.title}</h1>
+      <p style={{ marginTop: 8 }}>{data.description}</p>
+
+      <div style={{ marginTop: 16 }}>
         From <b>{data.from}</b> to <b>{data.to}</b>
-      </p>
+      </div>
     </div>
   );
 }
